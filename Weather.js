47d479ad438ee9d6dd4fcf120efc53d4b5c3c6bc
Choi-UngGyu <cdr982116@gmail.com>
2020-12-02 14:19:19 +0900

    2.3 Background Gradient

--- Weather.js
+++ Weather.js
@@ -1,18 +1,67 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
 import PropTypes from 'prop-types';
+import { LinearGradient } from 'expo-linear-gradient';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-export default function Weather({ temp }) {
+const weatherOptions = {
+    Haze: {
+        iconName: 'weather-hail',
+        gradient: ['#4DA0B0', '#D39D38']
+    },
+    Thunderstorm: {
+        iconName: '',
+        gradient: []
+    },
+    Drizzle: {
+        iconName: '',
+        gradient: []
+    },
+    Rain: {
+        iconName: '',
+        gradient: []
+    },
+    Snow: {
+        iconName: '',
+        gradient: []
+    },
+    Atmosphrer: {
+        iconName: '',
+        gradient: []
+    },
+    Clear: {
+        iconName: '',
+        gradient: []
+    },
+    Clouds: {
+        iconName: '',
+        gradient: []
+    },
+    Mist: {
+        iconName: '',
+        gradient: []
+    },
+    Dust: {
+        iconName: '',
+        gradient: []
+    }
+};
+
+export default function Weather({ temp, condition }) {
     return (
-        <View style={styles.container}>
+        <LinearGradient
+            colors={weatherOptions[condition].gradient}
+            style={styles.container}>
             <View style={styles.halfContainer}>
-                <MaterialCommunityIcons size={96} name="weather-lightning-rainy" />
-                <Text style={styles.temp}>{temp}o</Text>
+                <MaterialCommunityIcons
+                    size={96}
+                    name={weatherOptions[condition].iconName}
+                    color="white" />
+                <Text style={styles.temp}>{temp}°</Text>
             </View>
             <View style={styles.halfContainer}>
             </View>
-        </View>
+        </LinearGradient>
     );
 }
 
@@ -35,11 +84,12 @@ Weather.propTypes = {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     temp: {
-        fontSize: 42
+        fontSize: 42,
+        color: 'white'
     },
     halfContainer: {
         flex: 1,
